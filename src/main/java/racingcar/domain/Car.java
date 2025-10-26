8b0fe5c61a2cd8d2ceced544666c027e8df8f6d0
SeungGyu <phillipskl123@gmail.com>
2025-10-27 03:19:16 +0900

    feat: 자동차 상태 저장 기능 기능 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+public class Car {
+
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void attemptMove(int randomValue) {
+        if(randomValue >= 4) {
+            this.position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,35 @@
+package racingcar.domain;
+
+import racingcar.application.dto.response.CarStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> carList) {
+        this.cars = List.copyOf(carList);
+    }
+
+    public void attemptMoves(List<Integer> attemptValues) {
+        for (int i = 0; i < cars.size(); i++) {
+            cars.get(i).attemptMove(attemptValues.get(i));
+        }
+    }
+
+    public int maxPosition() {
+        return cars.stream().mapToInt(Car::getPosition).max().orElse(0);
+    }
+
+    public List<String> namesAtPosition(int target) {
+        return cars.stream().filter(car -> car.getPosition() == target)
+                .map(Car::getName).toList();
+    }
+
+    public List<CarStatus> reportStatuses() {
+        List<CarStatus> carStatuses = new ArrayList<>();
+        for (Car car : cars) carStatuses.add(new CarStatus(car.getName(), car.getPosition()));
+        return List.copyOf(carStatuses);
+    }
+}
