a05158dc974026f605cc53156cc34bcd6b52ca6b
SeungGyu <phillipskl123@gmail.com>
2025-10-27 00:40:24 +0900

    refactor: AppConfig final 변경

--- src/main/java/racingcar/config/AppConfig.java
+++ src/main/java/racingcar/config/AppConfig.java
@@ -3,26 +3,21 @@ package racingcar.config;
 import racingcar.controller.RacingGameController;
 import racingcar.application.RacingGameExecutor;
 import racingcar.application.RacingGameFacade;
-import racingcar.application.RacingGameSetUp;
 import racingcar.view.ApplicationConsoleView;
 import racingcar.view.ApplicationView;
 
-public class AppConfig {
+public final class AppConfig {
 
     public RacingGameController getRacingGameController() {
         return new RacingGameController(getApplicationView(), getRacingGameFacade());
     }
 
     private RacingGameFacade getRacingGameFacade() {
-        return new RacingGameFacade(getRacingGameSetUp(), getRacingGameExecutor());
+        return new RacingGameFacade(getRacingGameExecutor());
     }
 
     private RacingGameExecutor getRacingGameExecutor() {
         return new RacingGameExecutor();
-    }
-
-    private RacingGameSetUp getRacingGameSetUp() {
-        return new RacingGameSetUp();
     }
 
     private ApplicationView getApplicationView() {
